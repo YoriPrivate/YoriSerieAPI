2d43608b48984a599017864d53f1ed438b7c2475
Yori <stephanie.carrie06@gmail.com>
2021-07-07 21:40:29 +0200

    Ajout du projet Spring Boot + Implementation pom.xml

---
+++ src/test/java/com/yori/serie/SerieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yori.serie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SerieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
